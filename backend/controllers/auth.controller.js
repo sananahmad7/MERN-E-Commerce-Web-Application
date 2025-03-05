e2ae88b6ee72e3399975bea41dee8ed9560735f2
Sanan <sananahmad909@gmail.com>
2025-03-05 23:29:40 +0500

    Refresh token

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { redis } from "../lib/redis.js";
 import User from "../models/user.model.js";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 
 const generateTokens = (userId) => {
   const accessToken = jwt.sign({ userId }, process.env.TOKEN_SECRET, {
@@ -65,14 +66,85 @@ export const signup = async (req, res) => {
       message: "User created Successfully",
     });
   } catch (error) {
+    console.log("Error in signup controller : ", error.message);
+
     res.status(500).json({ message: error.message });
   }
 };
 
 export const login = async (req, res) => {
-  res.send("login route called");
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user && (await user.comparePassword(password))) {
+      const { accessToken, refreshToken } = generateTokens(user._id);
+
+      await storeRefreshToken(user._id, refreshToken);
+      setCookies(res, accessToken, refreshToken);
+
+      res.status(200).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      });
+    } else {
+      res.status(401).json({ message: "Invalid email or Password" });
+    }
+  } catch (error) {
+    console.log("Error in login controller : ", error.message);
+    res.status(500).json({ message: error.message });
+  }
 };
 
 export const logout = async (req, res) => {
-  res.send("logout route called");
+  try {
+    const refreshToken = req.cookies.refreshToken;
+    if (refreshToken) {
+      const decoded = jwt.verify(refreshToken, process.env.TOKEN_SECRET);
+      await redis.del(`refresh_token:${decoded.userId}`);
+    }
+    res.clearCookie("accessToken");
+    res.clearCookie("refreshToken");
+
+    res.status(200).json({ message: "Logout successful" });
+  } catch (error) {
+    console.log("Error in logout controller : ", error.message);
+
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
+
+//this will recreate the access token
+export const refreshToken = async (req, res) => {
+  try {
+    const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) {
+      res.status(401).json({ message: "No refresh token provided" });
+    }
+
+    const decoded = jwt.verify(refreshToken, process.env.TOKEN_SECRET);
+    const storedToken = await redis.get(`refresh_token:${decoded.userId}`);
+
+    if (storedToken !== refreshToken) {
+      res.status(401).json({ message: "Invalid refresh token" });
+    }
+
+    const accessToken = jwt.sign(
+      { userId: decoded.userId },
+      process.env.TOKEN_SECRET,
+      { expiresIn: "15m" }
+    );
+
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60,
+    });
+
+    res.json({ message: "Token refreshed" });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
 };
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 const app = express();
@@ -7,6 +8,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 app.use(express.json()); //allows you to parse body of the request
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
